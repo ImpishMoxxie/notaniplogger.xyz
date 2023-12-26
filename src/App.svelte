ec9b36308ad2594f8d35033eae2f7289f2cc014b
unknown <gplanet.online123@gmail.com>
2023-12-26 15:33:16 +0200

    update

--- src/App.svelte
+++ src/App.svelte
@@ -4,11 +4,34 @@
 
 <main>
     <center>
-        <h1>GAY SEX</h1>
-        <Sexcounter></Sexcounter>
+        <div class="funnyshape">
+            <img src="https://adu.shiggy.fun/v1/712653921692155965/avatar" alt="" class="img-avatar">
+            <h1 class="my-name">Moxxie</h1>
+            <h1>Just your casual imp (Not a POSSUM!)</h1>
+        </div>
     </center>
 </main>
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Rubik+Bubbles&display=swap');
 
+    .funnyshape{
+        margin-right: 25%;
+        margin-left: 25%;
+        border: 0.3em solid black;
+        border-radius: 25px;
+    }
+
+    .img-avatar{
+        margin-top: 3%;
+        border: 0.3em solid black;
+        border-radius: 25px;
+        display: flex;
+        align-items: flex-start;
+    }
+
+    .my-name{
+        font-family: "Rubik Bubbles";
+        font-size: 300%;
+    }
 </style>
