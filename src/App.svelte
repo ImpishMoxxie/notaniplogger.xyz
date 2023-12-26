32691508d37c730653eea74a6b35c44855f6bca7
unknown <gplanet.online123@gmail.com>
2023-12-26 14:33:27 +0200

    funny

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Sexcounter from "./lib/sexcounter.svelte";
+</script>
+
+<main>
+    <center>
+        <h1>GAY SEX</h1>
+        <Sexcounter></Sexcounter>
+    </center>
+</main>
+
+<style>
+
+</style>
---
+++ src/lib/sexcounter.svelte
@@ -0,0 +1,16 @@
+<script>
+    let count = 0;
+    function up (){
+        count+=1;
+    }
+</script>
+
+<main>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+    <h1 on:click = {up}>Echo body count: {count}</h1>
+</main>
+
+<style>
+
+</style>
